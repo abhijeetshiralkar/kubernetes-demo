a626473da9b281881cd6b4b29dc77325cf4b96f4
Abhijeet Shiralkar <abhijeet.shiralkar@asml.com>
2022-04-19 14:42:34 +0200

    Added logic to undeploy deployment

--- src/main/java/com/example/KuberentesdemoApplication.java
+++ src/main/java/com/example/KuberentesdemoApplication.java
@@ -9,6 +9,8 @@ import io.fabric8.kubernetes.client.KubernetesClient;
 
 /**
  * Refer https://github.com/fabric8io/kubernetes-client for more details about kubernetes client
+ * Refer https://itnext.io/difference-between-fabric8-and-official-kubernetes-java-client-3e0a994fd4af for difference between kubernetes official
+ * client vs one from fabric 8
  */
 @SpringBootApplication
 public class KuberentesdemoApplication {
--- src/main/java/com/example/service/KubernetesRunner.java
+++ src/main/java/com/example/service/KubernetesRunner.java
@@ -1,21 +1,15 @@
 package com.example.service;
 
 import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import io.fabric8.kubernetes.api.model.Namespace;
 import io.fabric8.kubernetes.api.model.NamespaceBuilder;
-import io.fabric8.kubernetes.api.model.NamespaceStatus;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.fabric8.kubernetes.api.model.apps.DeploymentBuilder;
 import io.fabric8.kubernetes.api.model.apps.DeploymentList;
-import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientException;
 
@@ -34,9 +28,9 @@ public class KubernetesRunner implements CommandLineRunner {
         //getListOfPods("vcp-facilities");
 
         //Create namespace
-        createNamespace("ingestion");
+        // createNamespace("ingestion");
 
-        createDeploymentInNamespace("twinscan-scheduler-3205", "ingestion");
+        // createDeploymentInNamespace("twinscan-scheduler-3205", "ingestion");
 
         //Get deployments for a namespace
         //getListOfDeployments("vcp-facilities");
@@ -44,6 +38,19 @@ public class KubernetesRunner implements CommandLineRunner {
         //Check if Deployment is available
         //checkifDeploymentIsRunning("commercialoptionmanager", "vcp-facilities");
 
+        // undeploy deployment
+        // undeployDeploymentFromNameSpace("twinscan-scheduler-3205", "ingestion");
+
+    }
+
+    private void undeployDeploymentFromNameSpace(String deploymentName, String nameSpace) {
+        System.out.println("Undeploying " + deploymentName + " from namespace " + nameSpace);
+        Boolean isDeleted = kubernetesClient.apps().deployments().inNamespace(nameSpace).withName(deploymentName).delete();
+        if (isDeleted) {
+            System.out.println("Deleted deployment " + deploymentName + " successfully");
+        } else {
+            System.out.println("Deletion of deployment failed " + deploymentName + " successfully");
+        }
     }
 
     private void createDeploymentInNamespace(String deploymentName, String nameSpace){
@@ -75,11 +82,10 @@ public class KubernetesRunner implements CommandLineRunner {
 
     private void checkifDeploymentIsRunning(String deploymentName, String namespace){
         System.out.println("Checking status for deployment " + deploymentName + " namespace "+ namespace);
-        DeploymentList deployments = kubernetesClient.apps().deployments().inNamespace(namespace).list();
-        Deployment matchingDeployment = deployments.getItems().stream().filter(deployment -> deployment.getMetadata().getName().equals(deploymentName)).findFirst().orElse(null);
-        boolean active = matchingDeployment.getStatus().getReadyReplicas() > 1 ? true : false;
+        Deployment deployment = kubernetesClient.apps().deployments().inNamespace(namespace).withName(deploymentName).get();
+        boolean active = deployment.getStatus().getReadyReplicas() > 1 ? true : false;
         System.out.println("Deployment " + deploymentName + "is active " + active);
-        System.out.println("Deployment status" + matchingDeployment.getStatus());
+        System.out.println("Deployment status" + deployment.getStatus());
     }
 
     private void getListOfDeployments(String namespace) {
