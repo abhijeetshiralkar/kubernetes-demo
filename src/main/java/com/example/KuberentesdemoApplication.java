1385308a9e98badcbfa8bf9bf77798eeabf89b43
Abhijeet Shiralkar <abhijeet.shiralkar@asml.com>
2022-04-19 11:55:42 +0200

    Added logic for getting list of deployments and also to check if a deployment is active

--- src/main/java/com/example/KuberentesdemoApplication.java
+++ src/main/java/com/example/KuberentesdemoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Bean;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 
+/**
+ * Refer https://github.com/fabric8io/kubernetes-client for more details about kubernetes client
+ */
 @SpringBootApplication
 public class KuberentesdemoApplication {
 
--- src/main/java/com/example/service/KubernetesRunner.java
+++ src/main/java/com/example/service/KubernetesRunner.java
@@ -1,5 +1,8 @@
 package com.example.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
@@ -8,6 +11,8 @@ import org.springframework.stereotype.Component;
 import io.fabric8.kubernetes.api.model.Namespace;
 import io.fabric8.kubernetes.api.model.NamespaceBuilder;
 import io.fabric8.kubernetes.api.model.NamespaceStatus;
+import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.api.model.apps.DeploymentList;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientException;
@@ -24,16 +29,41 @@ public class KubernetesRunner implements CommandLineRunner {
     @Override
     public void run(final String... args) throws Exception {
         //Get list of pods in a namespace
-        //getListOfPods();
+        //getListOfPods("vcp-facilities");
 
         //Create namespace
-        createNamespace();
+        //createNamespace("ingestion");
+
+        //Get deployments for a namespace
+        getListOfDeployments("vcp-facilities");
+
+        //Check if Deployment is available
+        checkifDeploymentIsRunning("commercialoptionmanager", "vcp-facilities");
+
     }
 
-    private void getListOfPods() {
-        System.out.println("Getting list of pods from namespace");
+    private void checkifDeploymentIsRunning(String deploymentName, String namespace){
+        System.out.println("Checking status for deployment " + deploymentName + " namespace "+ namespace);
+        DeploymentList deployments = kubernetesClient.apps().deployments().inNamespace(namespace).list();
+        Deployment matchingDeployment = deployments.getItems().stream().filter(deployment -> deployment.getMetadata().getName().equals(deploymentName)).findFirst().orElse(null);
+        boolean active = matchingDeployment.getStatus().getReadyReplicas() > 1 ? true : false;
+        System.out.println("Deployment " + deploymentName + "is active " + active);
+        System.out.println("Deployment status" + matchingDeployment.getStatus());
+    }
+
+    private void getListOfDeployments(String namespace) {
+        System.out.println("Getting list of deployments from namespace " + namespace);
+            DeploymentList deployments = kubernetesClient.apps().deployments().inNamespace(namespace).list();
+           deployments.getItems().forEach(deployment -> {
+               System.out.println(deployment.getMetadata().getName());
+           });
+    }
+
+
+    private void getListOfPods(String namespace) {
+        System.out.println("Getting list of pods from namespace " + namespace);
         try{
-            kubernetesClient.pods().inNamespace("vcp-facilities").list().getItems().forEach(
+            kubernetesClient.pods().inNamespace(namespace).list().getItems().forEach(
                     pod -> System.out.println(pod.getMetadata().getName())
             );
         } catch (KubernetesClientException ex) {
@@ -42,10 +72,10 @@ public class KubernetesRunner implements CommandLineRunner {
         }
     }
 
-    private void createNamespace(){
+    private void createNamespace(String namespace){
         Namespace ingestionNamespace = kubernetesClient.namespaces().create(new NamespaceBuilder()
                 .withNewMetadata()
-                .withName("ingestion")
+                .withName(namespace)
                 .endMetadata()
                 .build());
 
