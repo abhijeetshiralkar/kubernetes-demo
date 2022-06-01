afb599db7017e5521f92cb8004c6ddd1baee14c5
Abhijeet Shiralkar <abhijeet.shiralkar@asml.com>
2022-06-01 16:21:05 +0200

    Resolved nullpointer issue

--- src/main/java/com/example/KuberentesdemoApplication.java
+++ src/main/java/com/example/KuberentesdemoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.apis.AppsV1Api;
 import io.kubernetes.client.openapi.apis.CoreV1Api;
 import io.kubernetes.client.util.Config;
@@ -19,25 +18,18 @@ import io.kubernetes.client.util.Config;
 @SpringBootApplication
 public class KuberentesdemoApplication {
 
-    private ApiClient kubernetesApiClient;
-
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(KuberentesdemoApplication.class, args);
     }
 
     @Bean
-    ApiClient kubernetesApiClient() throws IOException {
-        return Config.defaultClient();
-    }
-
-    @Bean
     CoreV1Api kubernetesApi() throws IOException {
-        return new CoreV1Api(kubernetesApiClient);
+        return new CoreV1Api(Config.defaultClient());
     }
 
     @Bean
-    AppsV1Api kubernetesAppsApi() {
-        return new AppsV1Api(kubernetesApiClient);
+    AppsV1Api kubernetesAppsApi() throws IOException {
+        return new AppsV1Api(Config.defaultClient());
     }
 
 }
